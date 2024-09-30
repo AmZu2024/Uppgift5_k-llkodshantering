5c38ec4e74f7d28a1c505553358456038fcedcb6
AmZu2024 <amar.zukanovic57@gmail.com>
2024-09-30 10:31:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Enkelkalkylator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enkel Kalkylator");
+            Console.WriteLine("Ange aoperation (+, -, *, /): ");
+            char operation = Console.ReadKey().KeyChar;
+            Console.WriteLine("Ange det första talet: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ange det andra talet: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double result=0;
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    result = num1 / num2;
+                    break;
+
+                default:
+                    Console.WriteLine("Ogiltig operation.");
+                    break;
+            }
+            Console.WriteLine($"Resultat:{result}");
+
+          
+
+        }
+    }
+}
